506eaa60ff9e8c555118c4a69ac23497d827fec1
= <s.avinash.v@gmail.com>
2015-03-04 00:51:59 +0530

    simplify edgeClick detection by transforming coordinates

--- edgeClick.js
+++ edgeClick.js
@@ -1,39 +1,46 @@
-
 $(document).ready(function(){  
-  var $cols = $("tr").eq(0).children().length;  
-  $appx = Number(10), //click error tolerance
+  var $table = $("table.edge-click"),
+      bd_class = ["bd-top", "bd-bottom", "bd-left", "bd-right"];
+  $table.find("tr").first().find("td").addClass(bd_class[0]);
+  $table.find("tr").last().find("td").addClass(bd_class[1]);
+  $table.find("tr").each(function(){
+    $(this).find("td").first().addClass(bd_class[2]);
+    $(this).find("td").last().addClass(bd_class[3]);
+  });
+  var $cols = $table.find("tr").eq(0).children().length;  
+  $appx = 10, //click error tolerance
   $(".error").removeClass("init").hide();
   
-  $("td").on("click",function(e){
-    var $nowy = $("tr").index($(this).parent()),
-        $nowx = $("td").index($(this)),
+  $table.on("click", "td", function(e){
+    var $nowy = $table.find("tr").index($(this).parent()),
+        $nowx = $table.find("td").index($(this)),
         checkx = "",
-        checky = "";    
-        $wt = $(this).outerWidth(); //width of container
-        $ht = $(this).outerHeight(); //height of container
-        $left = e.pageX - $(this).offset().left, //click X posiotion inside cell
-        $top = e.pageY - $(this).offset().top, //click Y posiotion inside cell 
-        $cols = $("tr").eq(0).children().length;    
+        checky = "",    
+        $wt = $(this).outerWidth() - (2 * $appx), //width of container
+        $ht = $(this).outerHeight() - (2 * $appx), //height of container
+        $left = e.pageX - $(this).offset().left - $appx, //click X posiotion inside cell
+        $top = e.pageY - $(this).offset().top - $appx, //click Y posiotion inside cell 
+        $cols = $("tr").eq(0).children().length;
   
-    if(($left >= ($wt - $appx))&&($top >= $appx)&&($top <= ($ht - $appx))){      //right
+    if(($left >= $wt) && ($top >= 0) && ($top <= $ht)){      //right
       $(this).toggleClass("ec-right");
       $(this).next().toggleClass("ec-left");
     }
-    else if (($left <= $appx)&&($top >= $appx)&&($top <= ($ht - $appx))){      //left
+    else if (($left <= 0)&&($top >= 0)&&($top <= $ht)){      //left
       $(this).toggleClass("ec-left");
       $(this).prev().toggleClass("ec-right");
     }
-    else if (($top <= $appx)&&($left >= $appx)&&($left <= ($wt - $appx))){     //top
+    else if (($top <= 0)&&($left >= 0)&&($left <= $wt)){     //top
       $(this).toggleClass("ec-top");
       if($nowx>($cols-1)){
         $("td").eq($nowx-$cols).toggleClass("ec-bottom");
       }
     }
-    else if (($top >= ($ht - $appx))&&($left >= $appx)&&($left <= ($wt - $appx))){   //bottom   
+    else if (($top >= $ht)&&($left >= 0)&&($left <= $wt)){   //bottom   
       $(this).toggleClass("ec-bottom");
       $("td").eq($nowx+$cols).toggleClass("ec-top");
     }
-    else if (($top >= $appx)&&($left >= $appx)&&($left <= ($wt - $appx))&&($top <= ($ht - $appx))){      
+    else if (($top >= 0)&&($left >= 0)&&($left <= $wt)&&($top <= $ht)){      
       //nothing
     }
     else {
